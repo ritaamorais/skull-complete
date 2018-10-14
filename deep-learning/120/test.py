112d53fa1aa095c558b72f1632ddfda7b347a5c6
Ana Rita Morais <30924515+ritaamorais@users.noreply.github.com>
2018-10-14 16:58:53 +0100

    Updated 120/test.py

--- deep-learning/120/test.py
+++ deep-learning/120/test.py
@@ -6,53 +6,55 @@ import math
 import os
 
 import data_loader
-import hundredtwenty.net as net
+import net
 import torch
 import utils
 from scipy.io import savemat
 from torch import nn
 
-#TODO: corrigir estes paths
 parser = argparse.ArgumentParser()
-parser.add_argument('--test_data_dir', default='/home/morais/adapted/pycharm-skull-complete/data/test_data.mat', help="Path containing the testing dataset")
-parser.add_argument('--model_dir', default='/home/morais/adapted/pycharm-skull-complete/logs/', help="Directory containing the model")
+parser.add_argument('--i', default=25, help="Index of example instance for testing")
+parser.add_argument('--test_data_dir', default='./data/test_data.mat', help="Path containing the testing dataset")
+parser.add_argument('--model_dir', default='./logs/', help="Directory containing the model")
 
 
 def test_all(model, test_data, test_labels, tesize):
     """
-    -- takes all the test samples and outputs the completion and the average denoising error
+    Takes all the test samples and outputs the completion and the average denoising error
     """
 
     # set model to evaluation mode
     model.eval()
 
-    #load test data
     test_data.cuda()
 
-    inputs = torch.Tensor(tesize, 1, 30, 30, 30).cuda()
-    outputs = torch.Tensor(tesize, 1, 30, 30, 30).cuda()
-    perfect_cubes = torch.Tensor(tesize, 1, 30, 30, 30).cuda()
+    inputs = torch.Tensor(tesize, 1, 60, 60, 60).cuda()
+    outputs=torch.Tensor(tesize, 60*60*60).cuda()
+    perfect_cubes = torch.Tensor(tesize, 1, 60, 60, 60).cuda()
 
-    for k in range(tesize):
+	for k in range(tesize):
         input = test_data[k]
-        input = input.double()
+        input=input.reshape(1,1,60,60,60)
+        input = input.float()
+        input=input.cuda()
 
         perfect_input = test_labels[k]
         perfect_input = perfect_input.double()
 
         perfect_cubes[k] = perfect_input
         inputs[k] = input
+        output=model.forward(input)
+        outputs[k]=output
 
-    inputs=inputs.cuda()
-    outputs=model.forward(inputs)
-    outputs=outputs.double()
-    outputs=outputs.reshape(tesize, 1,30,30,30)
+    #inputs=inputs.cuda()
+    #outputs=model.forward(inputs)
+    #outputs=outputs.double()
+    outputs=outputs.reshape(tesize, 1,60,60,60)
 
-    #now that we have the output, estimate the error on the denoising task by only considering those voxels which were shut down at test time randomly
-    #error = reconstructed - original input
+    #After computing the outputs, estimate the error on the denoising task
+    #error = reconstructed - original inputt
     err = 0
 
-    #TODO: alterar este for para que os inputs sejam dados como (1,1,30,30,30) e não tudo de uma vez (222,1,30,30,30)
     for i in range(tesize):
         output = outputs[i]
 
@@ -62,10 +64,10 @@ def test_all(model, test_data, test_labels, tesize):
         perfect_cube = perfect_cubes[i]
         perfect_cube = perfect_cube.double()
         perfect_cube = perfect_cube.cuda()
-        noisey_voxels_tensor = torch.ne(bin_output, perfect_cube)  # this will give me a zero one tensor (1,30,30,30)indicating 1 where the cubes are equal in value and 0 otherwise; --ne means not equal (!=)
-        noisey_voxels_idx = torch.nonzero(noisey_voxels_tensor)  # this will give x by 4 (2 dims) matrix
+        noisey_voxels_tensor = torch.ne(bin_output, perfect_cube)  #This will give a binary tensor (1,30,30,30) indicating 1 where the cubes are equal in value and 0 otherwise; --ne means not equal (!=)
+        noisey_voxels_idx = torch.nonzero(noisey_voxels_tensor)  #matrix containing the voxels in which the reconstruction is different than the original
 
-        dummy = torch.numel(noisey_voxels_idx)  # numel counts the number of elements in the matrix "noisey_voxels_idx" - ou seja conta o numero de voxels em que a reconstrucao é diferente do original/correto
+        dummy = torch.numel(noisey_voxels_idx)  #numel counts the number of elements in the matrix "noisey_voxels_idx" 
 
         if dummy > 0:
             err = err + noisey_voxels_idx.size()[0]
@@ -75,44 +77,69 @@ def test_all(model, test_data, test_labels, tesize):
 
     aa = err/tesize
 
-    te_err = aa * 100 / 13824  # 13824 is the total number of voxels in the grid for this specific resolution (24x24x24=13824)
+    te_err = aa * 100 / 157464  # 157464 is the total number of voxels in the grid for this specific resolution (54x54x54=157464)
     return te_err
+
 
 def test_instance(model, i, test_data, test_labels, tesize):
     """
     this function is meant to feed the corrupted 3D test data to the network and save the output in binary format
---this output is then read and visualized in matlab.
+    -- this output can then be read and visualized in matlab.
+    -- computes the reconstruction error and the BCE loss for this instance
     """
+
     # set model to evaluation mode
     model.eval()
-
-    #MSELoss
-    loss_fn = nn.MSELoss()
-
-    test_labels = test_labels.reshape(tesize, 120*120*120)
+    
+    #error = reconstructed - original input
+    err = 0
+    
+    test_labels = test_labels.reshape(tesize, 60*60*60)
     model.encoder.__delitem__(0) #remove the dropout layer
 
-    inputs=torch.Tensor(1,1,120,120,120)
+    inputs=torch.Tensor(1,1,60,60,60)
     inputs=inputs.cuda()
 
     input=test_data[i]
     input=input.cuda()
     inputs[0]=input
 
-    target=test_labels[i]
+    perfect_cube=test_labels[i]
     outputs=model.forward(inputs)
     outputs=outputs.float()
-    target=target.float()
 
     outputs=outputs.cuda()
-    target=target.cuda()
+    
+    bin_output = torch.gt(outputs, 0.5)  # gt means >
+    bin_output = bin_output.double()
+    
+    perfect_cube = perfect_cube.double()
+    perfect_cube = perfect_cube.cuda()
+    noisey_voxels_tensor = torch.ne(bin_output, perfect_cube)
+    noisey_voxels_idx = torch.nonzero(noisey_voxels_tensor) 
 
-    err = loss_fn(outputs, target)
+    dummy = torch.numel(noisey_voxels_idx)
 
-    return err, outputs
+    if dummy > 0:
+        err = err + noisey_voxels_idx.size()[0]
+        err = (err/157464)*100
+    else:
+        print('no error in this example')
+    
+    perfect_cube=perfect_cube.float()
+    loss_fn = nn.BCELoss()
+    bce=loss_fn(outputs, perfect_cube)
+    
+    return err, bce, outputs
+
 
 def save_output(outputs, filename):
-    outputs=outputs.reshape(120,120,120)
+
+    """
+    Saves the reconstruction output in a .mat file
+    """
+
+    outputs=outputs.reshape(60,60,60)
     outputs=torch.squeeze(outputs)
 
     dims=outputs.ndimension()
@@ -122,6 +149,7 @@ def save_output(outputs, filename):
             outputs=torch.transpose(outputs, i, dims-i-1)
         outputs=outputs.contiguous()
 
+    outputs=outputs.data    
     outputs=outputs.cpu()
     np_outputs=outputs.numpy()
     np_outputs.astype(float)
@@ -132,17 +160,14 @@ def save_output(outputs, filename):
 if __name__ == '__main__':
 
     args = parser.parse_args()
-
-    # Set the random seed for reproducible experiments
-    #torch.manual_seed(230)
+    i=args.i
+    i=int(i)
 
     # Get the logger
     utils.set_logger(os.path.join(args.model_dir, 'evaluate.log'))
 
-    # Create the input data pipeline
+    #Load testing data
     logging.info("Loading the test dataset...")
-
-    # LOAD TESTING DATA
     test_data, tesize = data_loader.load_data(args.test_data_dir, 'dataset')
     logging.info("Number of testing examples: {}".format(tesize))
 
@@ -152,20 +177,18 @@ if __name__ == '__main__':
     autoencoder = net.VolAutoEncoder()
     autoencoder.cuda()
 
-
     #reload weights from the saved file
     utils.load_checkpoint(os.path.join(args.model_dir, 'last.pth.tar'), autoencoder)
 
-    #...and test!
-    #test all
+
+    #------test all-----------
     #te_err = test_all(autoencoder, test_data, test_labels, tesize)
-    #print('the test error is ' + str(te_err) + '%')
+    #logging.info("Test All: The test error is {} %".format(te_err))
 
 
-    #------test instance-----------
-    te_err, outputs=test_instance(autoencoder,145,test_data,test_labels,tesize)
-    logging.info("The test error is {} %".format(te_err))
+    #------test instance-----------w/ Recons Error and BCE loss
+    te_err, bce, outputs=test_instance(autoencoder,i,test_data,test_labels,tesize)
+    logging.info("Test instance {}: The reconstruction error is {} % and BCE Loss is {}".format(i,te_err,bce))
 
-    i=145
     filename= './recons'+str(i)
     save_output(outputs,filename)

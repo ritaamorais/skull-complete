48e05a5a7fad0d46c8aee2227e09a37a4defdb60
Ana Rita Morais <30924515+ritaamorais@users.noreply.github.com>
2018-10-14 16:57:43 +0100

    Updated 120/net.py

--- deep-learning/120/net.py
+++ deep-learning/120/net.py
@@ -1,15 +1,14 @@
-"""Defines the neural network, losss function and metrics"""
+"""Defines the neural network and the loss function"""
+
 
 import torch
 from torch import nn, optim
 
 class VolAutoEncoder(nn.Module):
     """
-       This is the standard way to define your own network in PyTorch. You typically choose the components
-       (e.g. LSTMs, linear layers etc.) of your network in the __init__ function.
-       You then apply these layers on the input step-by-step in the forward function.
-       You can use torch.nn.functional to apply functions such as F.relu, F.sigmoid, F.softmax.
-       Be careful to ensure your dimensions are correct after each step.
+       This is the standard way to define a network in PyTorch. The components
+       (layers) of the network are defined in the __init__ function.
+       Then, in the forward function it is defined how to apply these layers on the input step-by-step.
     """
 
     def __init__(self):
@@ -19,20 +18,20 @@ class VolAutoEncoder(nn.Module):
             nn.Dropout(p=0.5),  # dropout_P=0.5
             nn.Conv3d(1, 64, (9, 9, 9), stride=3),
             nn.ReLU(inplace=True),
-            nn.Conv3d(64, 256, (8, 8, 8), stride=4),
+            nn.Conv3d(64, 256, (6, 6, 6), stride=3),
             nn.ReLU(inplace=True),
         )
 
         self.linear = nn.Sequential(
-            nn.Linear(186624, 186624),
+            nn.Linear(32000, 32000),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),  # dropout_P=0.5
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose3d(256, 64, (5, 5, 5), stride=2),
+            nn.ConvTranspose3d(256, 64, (6, 6, 6), stride=3),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose3d(64, 1, (20, 20, 20), stride=5),
+            nn.ConvTranspose3d(64, 1, (9, 9, 9), stride=3),
         )
 
         self.sigmoid = nn.Sigmoid()
@@ -41,15 +40,16 @@ class VolAutoEncoder(nn.Module):
         """
         This function defines how we use the components of our network to operate on an input batch.
         """
-        encoded = self.encoder(x)
-        y = encoded.reshape(186624)
-        z = self.linear(y)
-        w = z.reshape((256, 9, 9, 9))
-        decoded = self.decoder(w)
-        r = decoded.reshape(1728000)  # featuresOut=inD*inD*inD=120*120*120=1728000
-        out = self.sigmoid(r)
+        x = self.encoder(x)
+        x = x.view(32000)
+        x = self.linear(x)
+        x = x.view((256, 5, 5, 5))
+        x=x.unsqueeze(0)
+        x = self.decoder(x)
+        x = x.view(216000)  #60*60*60=216000
+        x = self.sigmoid(x)
 
-        return out
+        return x
 
 def loss_fn(outputs, targets):
     """
@@ -58,11 +58,3 @@ def loss_fn(outputs, targets):
     loss = nn.BCELoss()
 
     return loss(outputs, targets)
-
-def err(outputs,labels): #denoising error
-    return 0
-
-#maintain all metrics required in this dictionary - these are used in the training and evaluation loops
-metrics = {
-    'err': err,
-}
